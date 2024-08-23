5949ed69a11f7c161aaf53379ba9d9d220418b31
Love Chaudhary <159931196+tech-chaudhary@users.noreply.github.com>
2024-08-23 18:44:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let count = 0;
+document.getElementById("count").innerHTML = count
+
+function increment(){
+    count = count + 1;
+    document.getElementById("count").innerHTML = count
+}
+
+function decrement(){
+    count = count - 1;
+    document.getElementById("count").innerHTML = count
+}
+
+let counter = 0;
+document.getElementById("counter").innerHTML = counter
+
+function incre(){
+    counter = counter + 1;
+    document.getElementById("counter").innerHTML = counter
+}
+
+function decre(){
+    counter = counter - 1;
+    document.getElementById("counter").innerHTML = counter
+}
+
+
+let counter2 = 0;
+document.getElementById("counter2").innerHTML = counter2
+
+function increm(){
+    counter2 = counter2 + 1;
+    document.getElementById("counter2").innerHTML = counter2
+}
+
+function decrem(){
+    counter2 = counter2 - 1;
+    document.getElementById("counter2").innerHTML = counter2
+}
